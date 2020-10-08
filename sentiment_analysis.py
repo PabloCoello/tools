88c3ca7d549ef72b8edf6822050e12c676a9da68
Pablo Coello <pablo.coello@outlook.com>
2020-10-08 23:16:40 +0200

    Removed extra dependencies

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -7,7 +7,6 @@ from rpy2.robjects.packages import importr
 from rpy2.robjects import pandas2ri
 from rpy2.robjects.conversion import localconverter
 import pandas as pd
-import numpy as np
 import unidecode
 
 def labmt_proc(row, labMT, labMTvector, labMTwordList):
