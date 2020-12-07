673611dd23c690a124d5a178a5e6e9ea5703c515
PabloC <pablo.coello@outlook.es>
2020-12-07 15:24:29 +0000

    Refact

--- km_tools.py
+++ km_tools.py
@@ -13,13 +13,12 @@ class km():
         '''
         self.x_ = df[features]
 
-
     def get_km__(self, n_clusters, max_iter):
         '''
         '''
         km = KMeans(n_clusters=n_clusters, max_iter=max_iter)
         return km.fit(self.x_)
-        
+
     def fit_km(self, n_clusters, max_iter):
         '''
         '''
@@ -50,7 +49,8 @@ class km():
         pca = PCA(n_components=2)
         pca_x = pca.fit_transform(self.x_)
         pca_x = pd.DataFrame(data=pca_x, columns=['comp_1', 'comp_2'])
-        pca_nombres_x = pd.concat([pca_x, pd.Series(self.km.labels_, name='km_labels')], axis=1)
+        pca_nombres_x = pd.concat(
+            [pca_x, pd.Series(self.km.labels_, name='km_labels')], axis=1)
 
         fig = plt.figure(1, figsize=(7, 7))
         ax = fig.add_subplot(1, 1, 1)
@@ -67,51 +67,35 @@ class km():
         '''
         return
 
+
 if __name__ == '__main__':
-    
+
     #Load test dataframe
     df = sb.load_dataset('iris')
     df.columns
-    
+
     #Instanciate class with df and feature columns
-    kmeans = km(df, ['sepal_length', 'sepal_width', 'petal_length', 'petal_width'])
-    
+    kmeans = km(df, ['sepal_length', 'sepal_width',
+                     'petal_length', 'petal_width'])
+
     #generate optimal number of clusters plot
     kmeans.get_optimal_nclust(40, 1000)
-    
+
     #Fit desired number of clusters
     kmeans.fit_km(3, 1000)
-    
+
     # Get labels from fit and store it in original df
     kmeans.get_labels()
     df['km_labels'] = kmeans.get_labels()
-    
+
     # Get centroids position
     kmeans.get_centroids()
-    
+
     # Get cluster plot
     kmeans.get_km_plot()
 
 
 '''
-#GRÁFICA CON EL PUNTO DE CODO DEL Nº DE CULSTERS
-
-Nc = range(1, 20)
-kmeans = [KMeans(n_clusters=i) for i in Nc]
-kmeans
-score = [kmeans[i].fit(X).score(X) for i in range(len(kmeans))]
-score
-plt.plot(Nc,score)
-plt.xlabel('Number of Clusters')
-plt.ylabel('Score')
-plt.title('Elbow Curve')
-plt.show()
-
-#APLICAR K-MEANS Y MOSTRAR CENTROIDES
-kmeans = KMeans(n_clusters=3).fit(X)
-centroids = kmeans.cluster_centers_
-print(centroids)
-
 def gen_matrix(niter, X):
     toret = []
     for i in range(niter):
